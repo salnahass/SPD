50a2e2d0cca1111f0bd000bfc0f1f04f7ba2382d
salnahas <salnahas@iu.edu>
2023-09-19 17:24:29 -0400

    Added dicts

---
+++ Assignment2Part1.py
@@ -0,0 +1,85 @@
+movies = [{
+"movieId": "1",
+"name": "Mission Impossible 14 - The AARP Threat",
+"pineappleMetric": "88",
+"releaseYear": "2018",
+"rating": "PG-13",
+"runtime": 136,
+"director": "Robert Silva",
+"writer": [
+"Mary Silva",
+"Mario Silva"
+],
+"productionCompany": {
+"prodCompanyName": "Rinse Wash Repeat",
+"prodCompanyLocation": "USA"
+},
+"budget": 127000000,
+"boxOffice": [32000000,15000000]
+},
+{
+"movieId": "2",
+"name": "Die Hard - Resurrection",
+"pineappleMetric": "58",
+"releaseYear": "2019",
+"rating": "R",
+"runtime": 117,
+"director": "Joan Smith",
+"writer": [
+"Terry Smith",
+"Francine Smith"
+],
+"productionCompany": {
+"prodCompanyName": "Yippee-Ki-Yay",
+"prodCompanyLocation": "Australia"
+},
+"budget": 63000000,
+"boxOffice": [19000000,7000000, 5300000]
+},
+{
+"movieId": "3",
+"name": "Fifty Shades Greener",
+"pineappleMetric": "35",
+"releaseYear": "2018",
+"rating": "PG",
+"runtime": 98,
+"director": "Cookie Muster",
+"writer": [
+"Elmo Jones",
+"Sue Bird"
+],
+"productionCompany": {
+"prodCompanyName": "Sesame Muppets",
+"prodCompanyLocation": "UK"
+},
+"budget": 17000000,
+"boxOffice": [
+58000000,
+43000000,
+27000000,
+13000000
+]
+}]
+reviews = [{
+"reviewId": "1",
+"reviewAuthor": "Timmy Twopence",
+"reviewDate": "2019-07-23",
+"reviewText": "It is an unexpected pleasure to have John McClane back as a zombie pursuing the nefarious masterminds behind a plan to dominate the world by transforming most of the population into undead slaves",
+"movieID": "2"
+},
+{
+"reviewId": "2",
+"reviewAuthor": "Sylvia Sterling",
+"reviewDate": "2018-06-12",
+"reviewText": "In this exciting movie, Ethan Hunt battles his worst enemy ever: a nefarious organization that advocates the retirement of actors when they are too old to play leads in action movies",
+"movieID": "1"
+},
+{
+"reviewId": "3",
+"reviewAuthor": "Francis Pounder",
+"reviewDate": "2018-12-04",
+"reviewText": "Kermit the Frog against a nefarious industrialist trying to pollute the world is a fun premise but the use of powerpoint slides during the movie is very distracting",
+"movieID": "3"
+}]
+
+
